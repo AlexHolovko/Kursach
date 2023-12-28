79c425d8985c3102e431be78890a8964ee93540d
golovko.sanya2 <golovko.sanya2@gmail.com>
2023-12-28 08:30:11 +0200

    значення за замовчуванням

--- Kursach/MainWindow.xaml.cs
+++ Kursach/MainWindow.xaml.cs
@@ -238,13 +238,13 @@ namespace Kursach
 
     public class User
     {
-        public int Id { get; set; }
-        public string? GuestName { get; set; }
-        public string? GuestEmail { get; set; }
-        public string? GuestPhone { get; set; }
-        public string? CheckInDate { get; set; }
-        public string? CheckOutDate { get; set; }
-        public string? RoomType { get; set; }
-        public string? RoomNumber { get; set; }
+        public int Id { get; set; } 
+        public string? GuestName { get; set; } = string.Empty;
+        public string? GuestEmail { get; set; } = string.Empty;
+        public string? GuestPhone { get; set; } = string.Empty;
+        public string? CheckInDate { get; set; } = string.Empty;
+        public string? CheckOutDate { get; set; } = string.Empty;
+        public string? RoomType { get; set; } = string.Empty;
+        public string? RoomNumber { get; set; } = string.Empty;
     }
 }
